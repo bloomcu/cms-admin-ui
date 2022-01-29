340fcf93343ced79392588a45c6ea6032a584c5e
Ryan Harmon <heyharmon@gmail.com>
2022-01-29 15:27:05 -0700

    Cleanup main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
 import Vue from 'vue'
-// import VueCompositionAPI, {createApp, h} from '@vue/composition-api'
-import VueCompositionAPI, {createApp, h} from '@vue/composition-api'
+import VueCompositionApi from '@vue/composition-api'
 import App from '@/App.vue'
-
-Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
-// new Vue({
-//   render: (h) => h(App),
-// }).$mount('#app');
+Vue.use(VueCompositionApi)
 
-const app = createApp({
-  render: () => h(App),
-})
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
 
-app.mount('#app')
+// const app = createApp({
+//   render: () => h(App),
+// })
+// app.mount('#app')
