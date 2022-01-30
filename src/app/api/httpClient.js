f511a70c51bb36d6d1cc0da2aba7215cdeece3bb
Ryan Harmon <heyharmon@gmail.com>
2022-01-29 23:08:43 -0700

    Install axios and create an httpClient wrapper for it

---
+++ src/app/api/httpClient.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+// import useAuth from '@/app/composables/useAuth'
+
+// TODO: Add token to all requests
+// const { token } = useAuth()
+
+/**
+ * CMS API Client
+ * An instance of Axios we can use for all API requests to CMS
+ */
+const httpClient = axios.create({
+    baseURL: 'http://cms.test/api/bloomcu/website',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        // 'Authorization': `Basic ${token}`
+        'Content-Type': 'application/json'
+    }
+})
+
+export { httpClient }
