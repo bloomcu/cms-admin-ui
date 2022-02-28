288a655a3a52f1045c5420ae91be1c5d321866c6
Ryan Harmon <heyharmon@gmail.com>
2022-02-28 03:33:33 -0700

    Add padding to editors

--- src/domain/blocks/components/editors/VideoBackgroundHeroEditor.vue
+++ src/domain/blocks/components/editors/VideoBackgroundHeroEditor.vue
@@ -92,6 +92,33 @@
           <!-- Add button -->
           <button @click="addButton()" class="btn btn--primary btn--small">Add Button</button>
         </div>
+        
+        <!-- Padding -->
+        <div class="form-group margin-bottom-sm">
+            <label class="form-label margin-bottom-xxs">Padding</label>
+            <div class="select margin-bottom-xxs">
+                <select v-model="block.data.config.paddingTop" name="paddingTop" id="paddingTop" class="select_input form-control width-100%">
+                    <option value="none" key="none" :selected="block.data.config.paddingTop === 'none'">Top: None</option>
+                    <option value="md" key="md" :selected="block.data.config.paddingTop === 'md'">Top: 1</option>
+                    <option value="lg" key="lg" :selected="block.data.config.paddingTop === 'lg'">Top: 2</option>
+                    <option value="xl" key="xl" :selected="block.data.config.paddingTop === 'xl'">Top: 3</option>
+                    <option value="xxl" key="xxl" :selected="block.data.config.paddingTop === 'xxl'">Top: 4</option>
+                    <option value="xxxl" key="xxxl" :selected="block.data.config.paddingTop === 'xxxl'">Top: 5</option>
+                </select>
+                <svg class="select__icon" aria-hidden="true" viewBox="0 0 16 16"><polyline points="1 5 8 12 15 5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/></svg>
+            </div>
+            <div class="select">
+                <select v-model="block.data.config.paddingBottom" name="paddingBottom" id="paddingBottom" class="select_input form-control width-100%">
+                    <option value="none" key="none" :selected="block.data.config.paddingBottom === 'none'">Bottom: None</option>
+                    <option value="md" key="md" :selected="block.data.config.paddingBottom === 'md'">Bottom: 1</option>
+                    <option value="lg" key="lg" :selected="block.data.config.paddingBottom === 'lg'">Bottom: 2</option>
+                    <option value="xl" key="xl" :selected="block.data.config.paddingBottom === 'xl'">Bottom: 3</option>
+                    <option value="xxl" key="xxl" :selected="block.data.config.paddingBottom === 'xxl'">Bottom: 4</option>
+                    <option value="xxxl" key="xxxl" :selected="block.data.config.paddingBottom === 'xxxl'">Bottom: 5</option>
+                </select>
+                <svg class="select__icon" aria-hidden="true" viewBox="0 0 16 16"><polyline points="1 5 8 12 15 5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/></svg>
+            </div>
+        </div>
     </div>
 </template>
 
